126249549978551745012b19fed63293074b4240
Ryan Douglas <ryan@luckydogdigital.com>
2016-02-23 10:31:54 -0700

    okay, I think I got this now

--- app.js
+++ app.js
@@ -1,49 +1,22 @@
 var express = require('express');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var bodyParser = require('body-parser');
+var socket = require('socket.io');
+var http = require('http');
+var unirest = require('unirest');
 
 var app = express();
+var server = http.Server(app);
+var io = socket(server);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+
 app.use(express.static('./frontend'));
 
+var port = process.env.PORT || 3000;
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
+server.listen(3000, function(){
+  console.log("Server listening on ", port);
+})
 
 
 module.exports = app;
